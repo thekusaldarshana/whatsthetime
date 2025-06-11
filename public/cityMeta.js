9a4c4495da8729a407dc5ed8d466f7d57d7af664
thekusaldarshana <thekusaldarshana@gmail.com>
2025-06-11 19:28:08 +0530

    expanded static pages to 14.7k

---
+++ public/cityMeta.js
@@ -0,0 +1,22 @@
+export const cityMeta = {
+  "florida": {
+    timeZone: "America/New_York",
+    dialingCode: "+1",
+    language: "English",
+    currency: "USD",
+    workHours: "9:00 AM - 5:00 PM",
+    weekend: "Saturday & Sunday",
+    season: "Summer",
+    bestTimeToVisit: "October to April"
+  },
+  "colombo": {
+    timeZone: "Asia/Colombo",
+    dialingCode: "+94",
+    language: "Sinhala, Tamil, English",
+    currency: "LKR",
+    workHours: "8:00 AM - 4:00 PM",
+    weekend: "Saturday & Sunday",
+    season: "Southwest Monsoon",
+    bestTimeToVisit: "January to March"
+  }
+};
